6f1cd0926bb89ff35a34b057cac6c98374b3f215
JorgeUndurraga <jorgeundurraga@yahoo.com>
2021-02-25 04:37:24 -0300

    Agregando Repository<Usuario, Integer>
    Se agregó Repository<Usuario, Integer> en: 
    public interface UsuarioRepository extends Repository<Usuario, Integer>, CrudRepository<Usuario, Integer>

--- src/main/java/com/latam/JPAcrud/dao/UsuarioRepository.java
+++ src/main/java/com/latam/JPAcrud/dao/UsuarioRepository.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
 import com.latam.JPAcrud.modelo.Usuario;
 
-public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+public interface UsuarioRepository extends  Repository<Usuario, Integer>, CrudRepository<Usuario, Integer> {
 	
 	@Query("FROM Usuario WHERE nombre = ?1 and clave = ?2")
 	public List<Usuario> findByNombreAndClave(String nombre, String clave);
